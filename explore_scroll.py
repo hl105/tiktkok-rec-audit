7a4791ff52aa7b622fbb66e1d7cd1866902888f8
Johanna <hl105@wellesley.edu>
2024-04-15 00:04:22 -0400

    loop code

--- explore_scroll.py
+++ explore_scroll.py
@@ -12,18 +12,34 @@ class TestTiktok(BaseCase):
     @pytest.mark.run(order=1)
     def test_manual_login(self):
         self.open('https://www.tiktok.com/explore')
-        time.sleep(30)
-    def test_save_html_page(self):
-        self.click(f'//*[@id="main-content-explore_page"]/div/div[1]/div[1]/button[{self.var1}]')
-        self.click('//*[@id="main-content-explore_page"]/div/div[2]/div/div[1]') #first vid
-        for i in range(0,20):
-            self.press_down_arrow()
-            time.sleep(random.randint(10,20))
-            self.send_keys('/html/body','l')
-            time.sleep(3)
-            if random.randint(1,10) == 1: #about once every 20 times
-                self.sendkeys('//body','l')
+        time.sleep(40)
+    def test_scroll(self):
+        last_hour = datetime.min
+        while True:
+            scroll = {'Society': 'Food', 'Singing': 'Singing', 'Sports':'Sports', 'Food':'Drama', 'Beauty':'Cars'}
+            self.open('https://www.tiktok.com/explore')
+            time.sleep(10)
+            self.scroll_into_view(f'span:contains("{scroll[self.var1]}")')
+            self.click(f'span:contains("{self.var1}")')
+            self.click('//*[@id="main-content-explore_page"]/div/div[2]/div/div[1]') #first vid
+            try:
+                self.press_down_arrow()
+                time.sleep( random.randint(10,20))
+                self.send_keys('/html/body','l')
+                time.sleep(3)
+                if random.randint(1,5) == 1: #about once every 20 times
+                    self.sendkeys('//body','l')
 
-        #instead of cron maybe just get current time?
-        #
+                current_time = datetime.now()
+                current_hour = current_time.hour
+                if current_hour != last_hour:
+                    self.open('https://www.tiktok.com/@funnycats0ftiktok/video/7326218472130694442?is_from_webapp=1&sender_device=pc&web_id=7340475663080785450')
+                    soup = self.get_beautiful_soup(source=None)
+                    with open(f"persona_{self.var1}/{current_hour}.html", "w") as file:
+                        file.write(str(soup))
+                    last_hour = current_hour
+                    time.sleep(100)
+            except:
+                pass
+        
                 
